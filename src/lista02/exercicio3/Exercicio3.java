a86d6659b23c6ba953e06db3379ac206ae04d0b4
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-09-04 23:02:16 -0300

    exercicio3 lista 2 otimizado e indentado

--- src/lista02/exercicio3/Exercicio3.java
+++ src/lista02/exercicio3/Exercicio3.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class Exercicio3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a sua comida favorita: ");
         String input = scanner.nextLine();
         scanner.close();
-
         System.out.println(palavraAoContrario(input));
     }
 
-    public static String palavraAoContrario(String comida){
-    char[] comidaAoContrario = new char[comida.length()];
+    public static String palavraAoContrario(String comida) {
+        char[] comidaAoContrario = new char[comida.length()];
 
-    int contador = 0;
-    for(int i = comida.length() - 1; i>=0; i--){
-        comidaAoContrario[contador] = comida.charAt(i);
-        contador++;
+        int contador = 0;
+        for (int i = comida.length() - 1; i >= 0; i--) {
+            comidaAoContrario[contador] = comida.charAt(i);
+            contador++;
         }
-    return new String(comidaAoContrario);
-    }}
+        return new String(comidaAoContrario);
+    }
+}
