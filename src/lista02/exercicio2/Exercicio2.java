f89f40147b44f3956541ee05e52668e8a1290564
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-09-04 22:50:52 -0300

    exercicio 2 da lista 2 finalizado

--- src/lista02/exercicio2/Exercicio2.java
+++ src/lista02/exercicio2/Exercicio2.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class Exercicio2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String string = pedirString(scanner);
-
+        String palavra = pedirString(scanner);
+        imprimirPrimeiraMetade(palavra);
+        scanner.close();
     }
 
     public static String pedirString(Scanner scanner){
@@ -13,5 +14,16 @@ public class Exercicio2 {
         return scanner.nextLine();
     }
 
+    public static void imprimirPrimeiraMetade(String palavra) {
+        int tamanho = palavra.length();
+        int metade = tamanho / 2;
 
+        System.out.println("A primeira metade da palavra é:");
+
+
+        for (int i = 0; i < metade; i++) {
+            System.out.print(palavra.charAt(i));
+        }
+        System.out.println();
+    }
 }
