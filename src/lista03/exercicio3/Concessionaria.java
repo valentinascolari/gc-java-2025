f23d19fb0e82cb347142bf66606aae814ffcafaa
Valentina <135928276+valentinascolari@users.noreply.github.com>
2025-09-11 23:11:51 -0300

    Update Concessionaria.java

--- src/lista03/exercicio3/Concessionaria.java
+++ src/lista03/exercicio3/Concessionaria.java
@@ -1,28 +1,43 @@
 package lista03.exercicio3;
 
 import lista03.exercicio2.Carro;
-import java.util.Scanner;
 
 public class Concessionaria {
-    private Carro[] armazenarCarro = new Carro[10];
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int quantidade = 0;
-        Carro carro = new Carro();
-        String listaCarros = listarCarros(carro);
+    private Carro[] carros = new Carro[10];
+    private int quantidade = 0;
 
-
-    }
-
-
-    public void adicionarCarro(Carro carro, int quantidade){
-        if(quantidade < armazenarCarro.length){
-            armazenarCarro[quantidade] = carro;
+    public void adicionarCarro(Carro carro) {
+        if (quantidade < carros.length) {
+            carros[quantidade] = carro;
             quantidade++;
+            System.out.println("Carro adicionado com sucesso!");
+        } else {
+            System.out.println("Não é possível adicionar mais carros. Capacidade máxima atingida.");
         }
-
     }
-    public static String listarCarros(Carro carro){
-        return "teste";
+    
+    public void listarCarros() {
+        if (quantidade == 0) {
+            System.out.println("Nenhum carro cadastrado.");
+        } else {
+            for (int i = 0; i < quantidade; i++) {
+                System.out.println("Carro " + (i + 1) + ":");
+                carros[i].exibirDados();
+                System.out.println("--------------------");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Concessionaria concessionaria = new Concessionaria();
+
+        Carro carro1 = new Carro("Vermelho", 30.5f, "XYZ-9876", true);
+        Carro carro2 = new Carro("Preto", 60.0f, "ABC-1234", false);
+
+        concessionaria.adicionarCarro(carro1);
+        concessionaria.adicionarCarro(carro2);
+
+        System.out.println("\nLista de carros cadastrados:");
+        concessionaria.listarCarros();
     }
 }
