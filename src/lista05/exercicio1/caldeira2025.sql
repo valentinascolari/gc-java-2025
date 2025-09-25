27747f62af469906c2216791e1c1a8958bc5b05a
Valentina <135928276+valentinascolari@users.noreply.github.com>
2025-09-25 16:28:30 -0300

    Update caldeira2025.sql

--- src/lista05/exercicio1/caldeira2025.sql
+++ src/lista05/exercicio1/caldeira2025.sql
@@ -1,3 +1,23 @@
+
+CREATE TABLE programacao_java.integrantes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cargo VARCHAR(100)
+);
+
+CREATE TABLE programacao_java.filmes_favoritos (
+    id SERIAL PRIMARY KEY,
+    nome_integrante VARCHAR(100) NOT NULL,
+    titulo_filme VARCHAR(255) NOT NULL,
+    genero VARCHAR(100)
+);
+
+
+INSERT INTO programacao_java.filmes_favoritos (nome_integrante, titulo_filme, genero) VALUES
+('Joao', 'Alien', 'Terror'),
+('Maria', 'O Chamado', 'Terror'),
+('Pedro', 'Avatar', 'Ficção Científica');
+
 SELECT * FROM programacao_java.filmes_favoritos;
 
 SELECT titulo_filme FROM programacao_java.filmes_favoritos
